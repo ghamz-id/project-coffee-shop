f5605e6f6a78e669d57086fb847590f9692d74b3
ghamz-id <hcoustic@gmail.com>
2024-03-14 11:36:10 +0700

    Setup redux

--- front-coffe/src/App.jsx
+++ front-coffe/src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home_Page from "./views/home_page";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
+	{
+		path: "/",
+		element: <Home_Page />,
+	},
 ]);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+	return (
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	);
 }
 
 export default App;
